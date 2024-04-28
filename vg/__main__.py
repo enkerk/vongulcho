91c483a7657d43fef8d365c0054334bec13b3a92
Nick <nkerklaan@gmail.com>
2024-04-27 19:52:41 -0600

    getting a basic tcod window working

--- vg/__main__.py
+++ vg/__main__.py
@@ -1,8 +1,22 @@
 """A traditional roguelike"""
+import tcod.context
+import tcod.tileset
 
 def main():
     """The main function (the fuck am I supposed to put here)"""
-    print("Hello world!")
+    tileset = tcod.tileset.load_tilesheet(
+        "vg/data/Alloy_curses_12x12.png", columns=16, rows=16, charmap=tcod.tileset.CHARMAP_CP437
+    )
+    tcod.tileset.procedural_block_elements(tileset=tileset)
+    console = tcod.console.Console(80, 50)
+    console.print(0, 0, "Hello World!")
+    with tcod.context.new(tileset=tileset) as context:
+        while True: # Main loop
+            context.present(console) # Render the console to the window
+            for event in tcod.event.wait(): # Event Loop, blocks until pending events exist
+                print(event)
+                if isinstance(event, tcod.event.Quit):
+                    raise SystemExit()
 
 if __name__ == "__main__":
     main()
